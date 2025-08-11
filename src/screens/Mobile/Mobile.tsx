eace8bad90aa431b596452cb684d7565996f205f
Shreyas Bhandary <168298221+shrey-bhandary@users.noreply.github.com>
2025-08-11 17:19:53 +0530

    Update Mobile.tsx

--- src/screens/Mobile/Mobile.tsx
+++ src/screens/Mobile/Mobile.tsx
@@ -181,7 +181,7 @@ export const Mobile = (): JSX.Element => {
               alt="Dr Devki Logo"
               className="h-6 w-auto object-contain"
             />
-            <span className="font-inter font-normal text-[#2b2b2b] text-base">
+            <span className="font-inter font-normal text-[#2b2b2b] text-sm">
               Dr. Devki Potwar
             </span>
           </div>
